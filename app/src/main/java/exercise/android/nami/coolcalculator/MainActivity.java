d76182fcd1385f47da586e4d89973ccd33e8ed97
Nataly Milman <n.pervusiak@gmail.com>
2021-06-11 23:34:58 +0300

    first commit

---
+++ app/src/main/java/exercise/android/nami/coolcalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package exercise.android.nami.coolcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
